eed3deb7e6fa8125c2923126443053abec8e48e9
Qirong <qirongwu18@gmail.com>
2026-01-14 18:52:37 -0500

    Modified component content

--- src/components/Education/Education.tsx
+++ src/components/Education/Education.tsx
@@ -1,5 +1,21 @@
 
 const Education = () => {
+
+    const educationInfo = [
+        {
+            school: "Stony Brook University",
+            location: "Stony Brook, NY",
+            degree: "Bachelor of Science in Computer Science",
+            years: "2021 - 2025"
+        },
+        {
+            school: "Manhattan Center for Science and Mathematics",
+            location: "New York, NY",
+            degree: "High School Diploma",
+            years: "2017 - 2021"
+        },
+    ];
+
     return (
         <>
         Education
