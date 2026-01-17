cf9ad1a9ca496f92700ba1da0ebcddca02c918ad
Qirong <qirongwu18@gmail.com>
2026-01-16 20:08:19 -0500

    Addressing build errors

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -1,30 +1,30 @@
-import { FaMapMarkerAlt } from 'react-icons/fa';
+// import { FaMapMarkerAlt } from 'react-icons/fa';
 
 const Experience = () => {
 
-    const experienceInfo = [
-        {
-            company: "Handshake",
-            location: "Remote",
-            position: "AI Fellow",
-            start: "December 2025",
-            end: "Present",
-        },
-        {
-            company: "LadySurvivor & Family Benches",
-            location: "New York, NY",
-            position: "Project Management Intern",
-            start: "July 2025",
-            end: "August 2025",
-        },
-        {
-            company: "SUNY - Stony Brook University",
-            location: "Stony Brook, NY",
-            position: "Undergraduate Teaching Assistant",
-            start: "August 2023",
-            end: "May 2024",
-        }
-    ]
+    // const experienceInfo = [
+    //     {
+    //         company: "Handshake",
+    //         location: "Remote",
+    //         position: "AI Fellow",
+    //         start: "December 2025",
+    //         end: "Present",
+    //     },
+    //     {
+    //         company: "LadySurvivor & Family Benches",
+    //         location: "New York, NY",
+    //         position: "Project Management Intern",
+    //         start: "July 2025",
+    //         end: "August 2025",
+    //     },
+    //     {
+    //         company: "SUNY - Stony Brook University",
+    //         location: "Stony Brook, NY",
+    //         position: "Undergraduate Teaching Assistant",
+    //         start: "August 2023",
+    //         end: "May 2024",
+    //     }
+    // ]
 
     return (
         <div className="max-w-4xl mx-auto px-4 py-20" id="Experience">
